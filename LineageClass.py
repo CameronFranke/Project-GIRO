e6fefe9100fadbc7a1e94a70b0cad5d37026eb17
CameronFranke <c_franke1@u.pacific.edu>
2015-11-13 12:22:52 -0800

    added yahoo finance support (only for testing & development)

--- LineageClass.py
+++ LineageClass.py
@@ -1,6 +1,8 @@
 __author__ = 'Cameron'
 import GiroUtilities as gu
 import talib
+import yahoo_finance as yf
+import urllib2
 
 class Lineage():
 
@@ -10,3 +12,29 @@ class Lineage():
         self.populationSize = populationSize
         self.population = []
         self.generationCount = generationCount
+        self.data = []
+
+    def pullYahooFinanceData(self):
+        '''
+        startDate = "2013-01-01"
+        endDate = "2014-12-30"
+        yahoo = yf.Share(self.symbol)
+        print yahoo.get_historical()
+        '''
+
+        base = "http://ichart.yahoo.com/table.csv?s="
+        base += self.symbol + "&a=1&b=1&c=2010"         #start date: month, day, year
+        base += "&d=12&e=31&f=2014"                     #ending data: month, day, year
+        base += "&g=d"                                  #interval
+        base += "&ignore=.cvs"                          #data format
+
+        response = urllib2.urlopen(base)
+        response = str(response.read())
+        response = response.splitlines()
+
+        for line in response:
+            line = line.split(",")
+            temp= {}
+            temp["date"] = line[0]
+            temp["price"] = line[6]
+            self.data.append(temp)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 __author__ = 'Cameron'
 import GiroUtilities as gu
+import LineageClass
 
 def main():
     gu.log("test")
+    x = LineageClass.Lineage("GOOG", 1, 1)
+    x.pullYahooFinanceData()
+
 
 
 main()
